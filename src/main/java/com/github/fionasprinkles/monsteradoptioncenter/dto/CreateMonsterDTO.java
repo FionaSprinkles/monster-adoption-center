2b209912fbfaae24204d0e9434db48db4b587fcf
Fiona Friberg <fionafriberg@outlook.com>
2026-03-18 09:11:53 +0100

    Initial code DTO

--- src/main/java/com/github/fionasprinkles/monsteradoptioncenter/dto/CreateMonsterDTO.java
+++ src/main/java/com/github/fionasprinkles/monsteradoptioncenter/dto/CreateMonsterDTO.java
@@ -1,4 +1,27 @@
 package com.github.fionasprinkles.monsteradoptioncenter.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
 public class CreateMonsterDTO {
+
+    @NotBlank
+    private String name;
+
+    private String species;
+
+    private String description;
+
+    @NotNull
+    private LocalDate arrivalDate;
+
+    @Min(0)@Max(10)
+    private int dangerLevel;
+
+    @Min(0)@Max(10)
+    private int tamedLevel;
 }
--- src/main/java/com/github/fionasprinkles/monsteradoptioncenter/dto/MonsterDTO.java
+++ src/main/java/com/github/fionasprinkles/monsteradoptioncenter/dto/MonsterDTO.java
@@ -1,5 +1,32 @@
 package com.github.fionasprinkles.monsteradoptioncenter.dto;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class MonsterDTO {
+
+    @NotBlank
+    private String name;
+
+    private String species;
+
+    private String description;
+
+    @NotNull
+    private LocalDate arrivalDate;
+
+    @Min(0)@Max(10)
+    private int dangerLevel;
+
+    @Min(0)@Max(10)
+    private int tamedLevel;
+
+    private boolean adopted;
 }
