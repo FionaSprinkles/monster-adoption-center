d66dd3c4803eebedf59527af5c6a1e5701ed1916
Fiona Friberg <fionafriberg@outlook.com>
2026-03-19 08:48:10 +0100

    Create MonsterServiceTest

---
+++ src/test/java/com/github/fionasprinkles/monsteradoptioncenter/service/MonsterServiceTest.java
@@ -0,0 +1,20 @@
+package com.github.fionasprinkles.monsteradoptioncenter.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void createMonster() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
