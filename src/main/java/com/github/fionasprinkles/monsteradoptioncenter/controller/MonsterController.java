ac67aaff0af6789fcefbca74d5f3f4388a52b235
Fiona Friberg <fionafriberg@outlook.com>
2026-03-20 11:28:33 +0100

    Add listAllMonsters, Add newMonster

--- src/main/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterController.java
+++ src/main/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterController.java
@@ -1,7 +1,11 @@
 package com.github.fionasprinkles.monsteradoptioncenter.controller;
 
+import com.github.fionasprinkles.monsteradoptioncenter.dto.CreateMonsterDTO;
+import com.github.fionasprinkles.monsteradoptioncenter.dto.MonsterDTO;
 import com.github.fionasprinkles.monsteradoptioncenter.service.MonsterService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +16,29 @@ public class MonsterController {
 
     public MonsterController(MonsterService monsterService) {
         this.monsterService = monsterService;
+    }
+
+    //TODO GET/POST för:
+    /**
+     * • Lista objekt
+     * • Visa formulär för nytt objekt
+     * • Skapa objekt
+     * • Visa formulär för uppdatering
+     * • Uppdatera objekt
+     * • Ta bort objekt
+     */
+
+    //Lista objekt
+    @GetMapping
+    public String listAllMonsters(Model model) {
+        model.addAttribute("monsters", monsterService.findAll());
+        return "monsters/list";
+    }
+
+    //Visa formulär för nytt objekt
+    @GetMapping("/new")
+    public String newMonster(Model model) {
+        model.addAttribute("createMonsterDTO", new CreateMonsterDTO());
+        return "monsters/create";
     }
 }
