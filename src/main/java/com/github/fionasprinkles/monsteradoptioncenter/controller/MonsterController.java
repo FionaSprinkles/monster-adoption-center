e305ef2cb6723f3320a5cd277816759263d4be8c
Fiona Friberg <fionafriberg@outlook.com>
2026-03-20 15:48:33 +0100

    Add newMonster, Rename newMonsterForm

--- src/main/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterController.java
+++ src/main/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterController.java
@@ -3,9 +3,13 @@ package com.github.fionasprinkles.monsteradoptioncenter.controller;
 import com.github.fionasprinkles.monsteradoptioncenter.dto.CreateMonsterDTO;
 import com.github.fionasprinkles.monsteradoptioncenter.dto.MonsterDTO;
 import com.github.fionasprinkles.monsteradoptioncenter.service.MonsterService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,15 +22,6 @@ public class MonsterController {
         this.monsterService = monsterService;
     }
 
-    //TODO GET/POST för:
-    /**
-     * • Lista objekt
-     * • Visa formulär för nytt objekt
-     * • Skapa objekt
-     * • Visa formulär för uppdatering
-     * • Uppdatera objekt
-     * • Ta bort objekt
-     */
 
     //Lista objekt
     @GetMapping
@@ -37,8 +32,24 @@ public class MonsterController {
 
     //Visa formulär för nytt objekt
     @GetMapping("/new")
-    public String newMonster(Model model) {
+    public String newMonsterForm(Model model) {
         model.addAttribute("createMonsterDTO", new CreateMonsterDTO());
-        return "monsters/create";
+        return "monsters/new";
     }
+
+    //Skapa objekt
+    @PostMapping
+    public String newMonster(@Valid @ModelAttribute CreateMonsterDTO createMonsterDTO, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "monsters/new";
+        } else {
+            monsterService.createMonster(createMonsterDTO);
+        }return "redirect:/monsters/list";
+    }
+
+    //Visa formulär för uppdatering
+
+    //Uppdatera objekt
+
+    //Ta bort objekt
 }
