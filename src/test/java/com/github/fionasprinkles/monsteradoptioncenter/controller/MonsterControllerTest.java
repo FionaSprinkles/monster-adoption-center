d9122192494692e4625499a244946df16f4f3f5a
Fiona Friberg <fionafriberg@outlook.com>
2026-03-23 11:48:45 +0100

    Add listMonsters

--- src/test/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterControllerTest.java
+++ src/test/java/com/github/fionasprinkles/monsteradoptioncenter/controller/MonsterControllerTest.java
@@ -1,8 +1,19 @@
 package com.github.fionasprinkles.monsteradoptioncenter.controller;
 
+import com.github.fionasprinkles.monsteradoptioncenter.dto.MonsterDTO;
+import com.github.fionasprinkles.monsteradoptioncenter.service.MonsterService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(MonsterController.class)
@@ -10,4 +21,21 @@ class MonsterControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private MonsterService monsterService;
+
+
+@DisplayName("Should return monster list page")
+@Test
+void listMonsters() throws Exception {
+
+    when(monsterService.findAll()).thenReturn(List.of(new MonsterDTO()));
+
+    mockMvc.perform(get("/monsters"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("monsters/list"))
+            .andExpect(model().attributeExists("monsters"));
+}
+
 }
