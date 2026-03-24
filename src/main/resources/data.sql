2ac8ee413d337a56b717776e4b6dd3df7a52e8df
Fiona Friberg <fionafriberg@outlook.com>
2026-03-24 07:52:25 +0100

    Add new monster data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,23 @@
 INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
 VALUES ('Fluffy', '/images/fluffy.png','Dogge', 'Loves music', '2026-03-22', 8, 8, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Mr. Wiggles', '/images/MrWiggles.png','Octopus', 'Needs a water tank', '2026-02-22', 8, 2, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Lil Frog', '/images/LilFrog.png','Frog', 'Loves hiphop music. Needs to find a home asp as we in the personal are tired of hearing him trying to rap all the time.', '2025-12-03', 1, 6, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Grumblehorn', '/images/Grumblehorn.png','Dogge', 'This little ball of fur looks dangerous but is actually not, we promise.','2026-01-30', 2, 8, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Heart and Curry', '/images/HeartAndCurry.png','Cat', 'Heart came to us with his little friend Curry. Needs to be adopted together.', '2026-03-13', 7, 3, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Drip', '/images/Drip.png','Liquid', 'Likes to stand in a corner and watch you all night long. Probably friendly', '2026-01-02', 4, 1, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Gibson', '/images/Gibson.png','Owl', 'Still a baby and needs lots of care. Makes a lot of noise at night.', '2026-03-20', 2, 2, false);
+
+INSERT INTO monster (name, image_url, species, description, arrival_date, danger_level, tamed_level, adopted)
+VALUES ('Bubbles', '/images/Bubbles.png','Dogge', 'Eats everything. He is not dangerous but if he gets hungry he will try to eat you.', '2026-02-18', 4, 5, false);
